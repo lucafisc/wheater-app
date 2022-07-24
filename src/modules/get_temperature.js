8fad79db3dea4e5413467d75d1fb3987480a015b
lucafisc <lucafischer@fastmail.com>
2022-07-24 16:46:11 +0200

    change fetch to https

--- src/modules/get_temperature.js
+++ src/modules/get_temperature.js
@@ -9,7 +9,7 @@ export const getTemperature = () => {
 const apikey = "9486ff66fc7c11980fc9ee8c66c285ca";
 
 async function getCurrent(city) {
-  const currentURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apikey}`;
+  const currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apikey}`;
 
   try {
     let response = await fetch(currentURL, { mode: "cors" });
