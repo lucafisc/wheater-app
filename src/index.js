aad920af56757d989de1953dde6d12be154692d6
lucafisc <lucafischer@fastmail.com>
2022-07-24 16:57:46 +0200

    Calculate vh correctly in mobile

--- src/index.js
+++ src/index.js
@@ -15,4 +15,9 @@ function init() {
   DOMRender();
 }
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
 init();
