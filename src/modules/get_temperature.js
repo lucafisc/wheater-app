2ad3298a73761b17f207a48205ad40e56e6898ce
lucafisc <lucafischer@fastmail.com>
2022-07-24 16:32:13 +0200

    Update info card

--- src/modules/get_temperature.js
+++ src/modules/get_temperature.js
@@ -2,17 +2,17 @@ import { pubsub } from "./pubsub.js";
 
 export const getTemperature = () => {
   pubsub.subscribe("new-city", (city) => {
-    getJson(city);
+    getCurrent(city);
   });
 };
 
-async function getJson(city) {
+const apikey = "9486ff66fc7c11980fc9ee8c66c285ca";
+
+async function getCurrent(city) {
+  const currentURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apikey}`;
+
   try {
-    let response = await fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&APPID=9486ff66fc7c11980fc9ee8c66c285ca"
-    );
+    let response = await fetch(currentURL);
     let json = await response.json();
     pubsub.publish("new-temperature", json);
   } catch (error) {
