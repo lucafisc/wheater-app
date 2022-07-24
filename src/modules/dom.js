c5d4b33da289f46e60395f56ebce716c4d087621
lucafisc <lucafischer@fastmail.com>
2022-07-24 17:20:37 +0200

    Fix mobile layout

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -4,6 +4,14 @@ const currentMinus = document.getElementById("current-minus");
 const currentSymbol = document.getElementById("current-symbol");
 
 export const DOMRender = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
   pubsub.subscribe("render", (tempList) => {
     newColor(tempList);
     showGradient();
